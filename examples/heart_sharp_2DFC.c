b4a54a7793ea49029c36f6ceb4c4beca14c74021
Allen Yang <allenyang@wavefield.cluster>
2025-11-19 16:25:37 -0800

    Saves code for getting figure results for sharp heart-shape

--- examples/heart_sharp_2DFC.c
+++ examples/heart_sharp_2DFC.c
@@ -70,9 +70,9 @@ int main() {
 
 
     double n_frac_c = 0.1;
-    double n_frac_S = 0.7;
+    double n_frac_S = 0.6;
 
-    double h = 0.00025;
+    double h = 0.0005;
     double h_tan = 5.5*h;
     double h_norm = h_tan;
     MKL_INT n_curve = ceil(3.5/h_tan);
